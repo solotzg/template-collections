e880b0e8739a8a7fb8f1251de720f76b9454f969
solotzg <solotzg@gmail.com>
2016-04-08 14:00:41 +0800

    union find

---
+++ data_structure/Template/union_find.cpp
@@ -0,0 +1,25 @@
+struct UnionFind {
+    vector<int> data;
+    void init(int n) {
+        data.assign(n, -1);
+    }
+    bool union_set(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if(x != y) {
+            if(data[y] < data[x]) swap(x, y);
+            data[x] += data[y];
+            data[y] = x;
+        }
+        return x != y;
+    }
+    bool find_set(int x, int y) {
+        return root(x) == root(y);
+    }
+    int root(int x) {
+        return data[x] < 0 ? x : data[x] = root(data[x]);
+    }
+    int size(int x) {
+        return -data[root(x)];
+    }
+};
