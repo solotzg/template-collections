3562dab4ffa1b98fe83bf070de8469ace78fec5d
Zhigao Tong <tongzhigao@pingcap.com>
2022-11-13 12:46:28 +0800

    update task scripts
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- .vscode/recompile.py
+++ .vscode/recompile.py
@@ -81,24 +81,19 @@ def wrap_run_time(func):
 
 
 @wrap_run_time
-def run_cmd(cmd, show_cmd=False):
-    by_shell = False if not isinstance(cmd, str) else True
-    if show_cmd:
-        str_cmd = cmd if by_shell else ' '.join(cmd)
-        logger.info("RUN CMD:\n{}\n".format(str_cmd))
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                            shell=by_shell, stderr=subprocess.PIPE)
-    stdout, stderr = proc.communicate()
-    return stdout, stderr, proc.returncode
-
-
-def run_shell_cmd(cmd, show_cmd=True):
-    stdout, stderr, err = run_cmd(cmd, show_cmd)
-    stdout, stderr = stdout.decode('utf-8'), stderr.decode('utf-8')
-    if err:
-        error_msg = "stdout: `{}`, stderr: `{}`".format(stdout, stderr)
-        raise Exception("{}".format(error_msg))
-    return stdout.split('\n')
+def run_cmd(cmd, show_stdout=False):
+    logger.debug("RUN CMD:\n\t{}\n".format(cmd))
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout = None
+    if show_stdout:
+        stdout = bytes()
+        for line in proc.stdout:
+            stdout += (line)
+            logger.debug(line.decode('utf-8').rstrip())
+    _stdout, stderr = proc.communicate()
+    stdout = stdout if show_stdout else _stdout
+    return stdout.decode('utf-8'), stderr.decode('utf-8'), proc.returncode
 
 
 class Runner:
@@ -109,59 +104,115 @@ class Runner:
         parser.add_argument(
             '--file', help='absolute path of file to compile', required=True)
         parser.add_argument(
-            '--type', help='build type', required=True, choices=('syntax', 'tidy', 'build'))
+            '--type', help='build type', required=True, choices=('syntax', 'tidy', 'build', 'objdump'))
         parser.add_argument(
-            '--time-trace', help='enable time trace', action='store_true')
+            '--time-trace', help='enable time trace, will generate json profile', action="store_true",)
+        parser.add_argument(
+            '--work-dir', help='current work directory', required=True)
+        parser.add_argument(
+            '--clang-tidy-path', help='clang-tidy path',)
+        parser.add_argument(
+            '--release-build', help='use release build compile flags', action="store_true")
         self.args = parser.parse_args()
-        self.repo_path = os.getcwd()
-        self.file_path = self.args.file
+        self.work_dir = self.args.work_dir
+        os.chdir(self.work_dir)
         self.compile_commands_set = json.load(
-            open("{}/{}".format(self.repo_path, self.compile_commands_path), "r"))
+            open("{}/{}".format(self.work_dir, self.compile_commands_path), "r"))
 
     def run(self):
         if self.args.type == 'syntax':
-            self.run_recompile_syntax(True)
+            self.run_recompile(True,)
         elif self.args.type == 'tidy':
             self.run_clang_tidy()
         elif self.args.type == 'build':
-            self.run_recompile_syntax(False, self.args.time_trace)
+            self.run_recompile(False,)
+        elif self.args.type == 'objdump':
+            self.run_objdump()
         else:
             exit(-1)
 
     def run_clang_tidy(self):
-        cmd = "clang-tidy"
-        args = [
-            "-p",
-            "{}/{}".format(self.repo_path, self.compile_commands_path),
-            self.file_path,
-        ]
-        res = run_shell_cmd("{} {}".format(cmd, ' '.join(args)), show_cmd=True)
-        logger.error(''.join(res))
+        assert self.args.clang_tidy_path
+        cmd = "{} -p {}/{} {}".format(
+            self.args.clang_tidy_path,
+            self.work_dir, self.compile_commands_path, self.args.file)
 
-    def run_recompile_syntax(self, syntax_only=True, time_trace=False):
-        suffix = " -fsyntax-only" if syntax_only else ""
-        if time_trace:
-            suffix += " -ftime-trace"
+        stdout, stderr, status = run_cmd(cmd)
+        if status:
+            logger.info(stdout)
+            logger.error(stderr)
+            exit(status)
+
+    def find_compile_block(self, file_path):
         for a in self.compile_commands_set:
             f = a.get("file")
-            if f != self.file_path:
-                continue
-            cmd = a.get("command")
-            logger.info(".")
-            logger.info("..")
-            logger.info("...")
-            logger.info("... Compiling `{}` ...".format(self.file_path))
-            run_shell_cmd(cmd + suffix, )
-            logger.info(
-                "... Compilation of '{}' finished ...".format(self.file_path))
-            return
-        logger.error("{} not found in {}".format(
-            self.file_path, self.compile_commands_path))
-        exit(-1)
+            if f == file_path:
+                return a
+        raise Exception("{} not found in {}".format(
+            file_path, self.compile_commands_path))
+
+    def run_objdump(self):
+        cmd: str = self.find_compile_block(self.args.file).get("command")
+        cmd: list = cmd.split(' ')
+        obj_index = cmd.index('-o')
+        obj_file = None
+        for i in range(obj_index+1, len(cmd)):
+            if cmd[i]:
+                obj_file = cmd[i]
+                break
+        assert obj_file.endswith(".o")
+
+        saved_file = "/tmp/.tzg_objdump_{}.{}.txt".format(
+            self.work_dir.replace('/', '.'),
+            obj_file.replace('/', '.')
+        )
+        cmd = "objdump -C -d {}/{} > {}".format(
+            self.work_dir, obj_file, saved_file)
+
+        logger.info(".")
+        logger.info("..")
+        logger.info("...")
+        logger.info("... Objdump `{}` ...".format(obj_file))
+        _, stderr, status = run_cmd(cmd)
+        if status:
+            logger.error(stderr)
+            exit(status)
+        logger.info(
+            "... Objdump of '{}' finished ...".format(obj_file))
+        logger.info(
+            "... Please open file: `{}`".format(saved_file))
+
+    def run_recompile(self, syntax_only=True, ):
+        suffix = " -fsyntax-only" if syntax_only else ""
+        if self.args.time_trace:
+            suffix += ' -ftime-trace'
+
+        cmd: str = self.find_compile_block(self.args.file).get("command")
+
+        if self.args.release_build:
+            suffix += " -O3 -DNDEBUG -fno-verbose-asm -finline-functions"
+            cmd = cmd.replace("-fprofile-instr-generate", "")
+            cmd = cmd.replace("-fcoverage-mapping", "")
+
+        cmd: str = self.find_compile_block(self.args.file).get("command")
+        logger.info(".")
+        logger.info("..")
+        logger.info("...")
+        logger.info("... Compiling `{}` ...".format(self.args.file))
+        _, stderr, status = run_cmd(cmd + suffix, )
+        if status:
+            logger.error(stderr)
+            exit(-1)
+        logger.info(
+            "... Compilation of '{}' finished ...".format(self.args.file))
 
 
 def main():
-    Runner().run()
+    try:
+        Runner().run()
+    except Exception as e:
+        logger.error(e)
+        exit(-1)
 
 
 if __name__ == '__main__':
