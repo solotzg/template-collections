90219ed3c4a1b36cd37923916e0cba802e5cd5ef
Zhigao Tong <tongzhigao@pingcap.com>
2023-01-20 22:00:17 +0800

    update .vscode/run-code-coverage.sh
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- .vscode/run-code-coverage.sh
+++ .vscode/run-code-coverage.sh
@@ -32,7 +32,7 @@ CC=${CC} CXX=${CXX} cmake -S ${WORKSPACE_FOLDER} \
     -DTEST_LLVM_COVERAGE=ON \
     -GNinja
 
-rm -rf ${WORKSPACE_FOLDER}/cmake-build-debug/CPPTest
+BIN=${WORKSPACE_FOLDER}/cmake-build-debug/CPPTest
 cmake --build . --parallel ${NPROC}
 
 LCOV_PATH=$(which lcov)
@@ -43,7 +43,9 @@ rm -rf ${PROFILE_DIR}
 rm -rf ${REPORT_DIR}
 
 export LLVM_PROFILE_FILE=${PROFILE_DIR}/code-cov-%12m.profraw
-${WORKSPACE_FOLDER}/cmake-build-debug/CPPTest
+
+${BIN}
+
 llvm-profdata merge -output=${PROFILE_DIR}/merged.profdata ${PROFILE_DIR}/code-cov-*.profraw
 cd ${PROFILE_DIR}
 llvm-cov export ${WORKSPACE_FOLDER}/cmake-build-debug/CPPTest --format=lcov \
