686a7964f3e8f3920a8c98ec61061a0197126c09
Zhigao Tong <tongzhigao@pingcap.com>
2023-01-24 10:37:11 +0800

    update cmake scripts
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

---
+++ cmake/linker.cmake
@@ -0,0 +1,42 @@
+
+# Linker
+string (REGEX MATCHALL "[0-9]+" COMPILER_VERSION_LIST ${CMAKE_CXX_COMPILER_VERSION})
+list (GET COMPILER_VERSION_LIST 0 COMPILER_VERSION_MAJOR)
+
+# Example values: `lld-10`, `gold`.
+option (LINKER_NAME "Linker name or full path")
+
+find_program (LLD_PATH NAMES "lld${COMPILER_POSTFIX}" "lld")
+find_program (GOLD_PATH NAMES "ld.gold" "gold")
+if (OS_DARWIN)
+    find_program (LD_PATH NAMES "ld")
+endif ()
+
+if (NOT LINKER_NAME)
+    if (OS_DARWIN AND COMPILER_APPLE_CLANG)
+        set (LINKER_NAME "ld")
+    elseif (LLD_PATH)
+        set (LINKER_NAME "lld")
+    elseif (GOLD_PATH)
+        message (WARNING "Linking with gold is not recommended. Please use lld.")
+        set (LINKER_NAME "gold")
+    endif ()
+endif ()
+
+if (LINKER_NAME)
+    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINKER_NAME}")
+    message(STATUS "Using linker: ${LINKER_NAME}")
+else()
+    message(STATUS "Using linker: <default>")
+endif()
+
+option(ENABLE_THINLTO "Clang-specific link time optimization" OFF)
+
+if (ENABLE_THINLTO)
+    # Link time optimization
+    set (THINLTO_JOBS "0" CACHE STRING "ThinLTO compilation parallelism")
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -flto=thin -fvisibility=hidden -fvisibility-inlines-hidden -fsplit-lto-unit")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto=thin -fvisibility=hidden -fvisibility-inlines-hidden -fwhole-program-vtables -fsplit-lto-unit")
+    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -flto=thin -flto-jobs=${THINLTO_JOBS} -fvisibility=hidden -fvisibility-inlines-hidden -fwhole-program-vtables -fsplit-lto-unit")
+    message (STATUS "Using `ThinLTO`, lto-jobs=${THINLTO_JOBS}")
+endif ()
--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -7,37 +7,6 @@ elseif (CMAKE_SYSTEM_NAME MATCHES "Darwin")
     set (OS_DARWIN 1)
 endif ()
 
-# Linker
-string (REGEX MATCHALL "[0-9]+" COMPILER_VERSION_LIST ${CMAKE_CXX_COMPILER_VERSION})
-list (GET COMPILER_VERSION_LIST 0 COMPILER_VERSION_MAJOR)
-
-# Example values: `lld-10`, `gold`.
-option (LINKER_NAME "Linker name or full path")
-
-find_program (LLD_PATH NAMES "lld${COMPILER_POSTFIX}" "lld")
-find_program (GOLD_PATH NAMES "ld.gold" "gold")
-if (OS_DARWIN)
-    find_program (LD_PATH NAMES "ld")
-endif ()
-
-if (NOT LINKER_NAME)
-    if (OS_DARWIN AND COMPILER_APPLE_CLANG)
-        set (LINKER_NAME "ld")
-    elseif (LLD_PATH)
-        set (LINKER_NAME "lld")
-    elseif (GOLD_PATH)
-        message (WARNING "Linking with gold is not recommended. Please use lld.")
-        set (LINKER_NAME "gold")
-    endif ()
-endif ()
-
-if (LINKER_NAME)
-    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINKER_NAME}")
-    message(STATUS "Using linker: ${LINKER_NAME}")
-else()
-    message(STATUS "Using linker: <default>")
-endif()
-
 # Arch
 if (CMAKE_SYSTEM_PROCESSOR MATCHES "^(aarch64.*|AARCH64.*|arm64.*|ARM64.*)")
     set (ARCH_AARCH64 1)
@@ -47,6 +16,79 @@ if (CMAKE_SYSTEM_PROCESSOR MATCHES "^(amd64.*|AMD64.*|x86_64.*|X86_64.*)")
 endif ()
 if (ARCH_AARCH64 OR CMAKE_SYSTEM_PROCESSOR MATCHES "arm")
     set (ARCH_ARM 1)
+endif ()
+
+# Time trace
+option(ENABLE_TIME_TRACES "enable clang feature time traces" OFF)
+
+if(ENABLE_TIME_TRACES)
+    set(CLANG_TIME_TRACES_FLAGS "-ftime-trace")
+    message(STATUS "Using clang time traces flag `${CLANG_TIME_TRACES_FLAGS}`")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CLANG_TIME_TRACES_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CLANG_TIME_TRACES_FLAGS}")
+endif()
+
+# PCH
+function(add_target_pch context target)
+    if (ENABLE_PCH)
+        message(STATUS "Add PCH `${context}` for target `${target}`")
+        target_precompile_headers(${target} PRIVATE ${context})
+    endif ()
+    if(${ARGC} GREATER 2)
+        add_target_pch(${context} ${ARGN})
+    endif()
+endfunction()
+
+option (ENABLE_PCH "Enable `Precompiled header`" ON)
+if (OS_DARWIN AND ARCH_ARM)
+    message (STATUS "Disable `Precompiled header` for ${CMAKE_SYSTEM_NAME} ${CMAKE_SYSTEM_PROCESSOR}")
+    set (ENABLE_PCH OFF)
+endif ()
+
+# CCACHE
+option (USE_CCACHE "Set to OFF to disable ccache" OFF)
+if (USE_CCACHE)
+    find_program (CCACHE_FOUND ccache)
+endif ()
+if (USE_CCACHE AND CCACHE_FOUND AND NOT CMAKE_CXX_COMPILER_LAUNCHER MATCHES "ccache" AND NOT CMAKE_CXX_COMPILER MATCHES "ccache")
+    execute_process (COMMAND ${CCACHE_FOUND} "-V" OUTPUT_VARIABLE CCACHE_VERSION)
+    execute_process (COMMAND ${CCACHE_FOUND} "-p" OUTPUT_VARIABLE CCACHE_CONFIG)
+    string (REGEX REPLACE "ccache version ([0-9\\.]+).*" "\\1" CCACHE_VERSION ${CCACHE_VERSION})
+    message (STATUS "Using ccache: ${CCACHE_FOUND}, version ${CCACHE_VERSION}")
+    message (STATUS "Show ccache config:")
+    message ("${CCACHE_CONFIG}")
+    set_property (GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_FOUND})
+    set_property (GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_FOUND})
+
+    if (ENABLE_PCH)
+        execute_process (COMMAND ${CCACHE_FOUND} --get-config sloppiness OUTPUT_VARIABLE _CCACHE_SLOPPINESS OUTPUT_STRIP_TRAILING_WHITESPACE)
+        string (FIND "${_CCACHE_SLOPPINESS}" "pch_defines" _CCACHE_SLOPPINESS_RES)
+        if (NOT _CCACHE_SLOPPINESS_RES STREQUAL "-1")
+            string (FIND "${_CCACHE_SLOPPINESS}" "time_macros" _CCACHE_SLOPPINESS_RES)
+        endif ()
+
+        if (_CCACHE_SLOPPINESS_RES STREQUAL "-1")
+            message(WARNING "`Precompiled header` won't be cached by ccache, sloppiness = `${CCACHE_SLOPPINESS}`,please execute `ccache -o sloppiness=pch_defines,time_macros`")
+            set (ENABLE_PCH FALSE CACHE BOOL "" FORCE)
+        endif ()
+    endif ()
+else ()
+    message (STATUS "Not using ccache ${CCACHE_FOUND}, USE_CCACHE=${USE_CCACHE}")
+endif ()
+
+# Use arch native
+option (ARCHNATIVE "Enable `-march=native` compiler flag" OFF)
+if (ARCHNATIVE AND NOT OS_DARWIN)
+    message(STATUS "Using `-march=native` compiler flag")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=native")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
+endif ()
+
+# Code coverage
+option (TEST_LLVM_COVERAGE "Enables flags for test coverage" OFF)
+if (TEST_LLVM_COVERAGE)
+    set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping")
+    set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping")
 endif ()
 
 # Other
