2e20c54894dfca9752945bf889ccd0fdcc9cf514
Zhigao Tong <tongzhigao@pingcap.com>
2023-06-19 18:23:40 +0800

    remove `to_log_msg` from `pybind_utils/src/lib.cpp`
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- pybind_utils/src/lib.cpp
+++ pybind_utils/src/lib.cpp
@@ -1,17 +1,9 @@
 #include "prime_helper.h"
 #include "pybind_utils/pch.h"
 
-namespace {
-std::string to_log_msg(std::string_view &&msg) {
-  auto &&s = TO_LOG_MSG((msg));
-  return s;
-}
-} // namespace
-
 PYBIND11_MODULE(pybind_utils, m) {
   m.doc() = "pybind11 plugin `pybind_utils`";
-  m.def("to_log_msg", &to_log_msg,
-        "A function that make message into log message");
+  m.def("get_tid", &utils::get_tid);
   m.def("gen_prime_helper_with_maxlen", gen_prime_helper_with_maxlen);
   m.def("gen_prime_helper_with_maxnum", gen_prime_helper_with_maxnum);
   m.def("prime_helper_init_pi_small", prime_helper_init_pi_small);
