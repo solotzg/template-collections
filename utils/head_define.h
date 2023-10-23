618acd1ae54dc92f04ed9a531cea7fbdf789f78e
Zhigao Tong <solotzg@163.com>
2023-10-23 23:35:35 +0800

    update utils/head_define.h

--- utils/head_define.h
+++ utils/head_define.h
@@ -31,6 +31,10 @@ typedef uint64_t U64;
 typedef int64_t I64;
 typedef uint32_t U32;
 typedef int32_t I32;
+typedef uint16_t U16;
+typedef int16_t I16;
+typedef uint8_t U8;
+typedef int8_t I8;
 
 #define likely(x) (__builtin_expect(!!(x), 1))
 #define unlikely(x) (__builtin_expect(!!(x), 0))
